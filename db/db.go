2e9ecd90bcb9c427a918688412a8bc2acd611c30
ceqi <qiceforuni@gmail.com>
2021-05-26 22:08:16 +0800

    setup db client

--- db/db.go
+++ db/db.go
@@ -5,27 +5,23 @@ import (
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func connectmdb() int64 {
+func CreatedbClient() (client *mongo.Client, ctx context.Context, err error) {
 
 	uri := "mongodb+srv://cluster0.tmbdu.mongodb.net/crawlerdb?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=/Users/ce/dev/certs/X509-cert-4795979462624466445.pem"
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	client, err = mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	defer client.Disconnect(ctx)
-
-	collection := client.Database("crawlerdb").Collection("links")
-
-	docCount, err := collection.CountDocuments(ctx, bson.D{})
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
+		client.Disconnect(ctx)
 	}
-	return docCount
 
+	return
 }
